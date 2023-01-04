a6d658831f4d5c80b9aa8adae2f5f9455e5a7c84
Paola <paola.vidal@edu.cmeducations.se>
2023-01-04 13:47:36 +0100

    first push

---
+++ js/api-java.js
@@ -0,0 +1,26 @@
+$(function() {
+
+    const RICKANDMORTY = "https://rickandmortyapi.com/api/character/";
+
+    $(".btn").on("click", testingApi);
+
+    function testingApi() {
+        let id = Math.floor(Math.random() * 826);
+
+        
+        fetch(RICKANDMORTY + id)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+
+            $(".character-name").text(data.name);
+            $(".image").attr("src", data.image);
+
+            $(".location").text(data.location.name);
+            $(".gender").text(data.gender);
+            $(".status").text(data.status);
+
+    })
+  }
+
+})
